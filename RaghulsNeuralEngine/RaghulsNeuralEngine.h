8870eda0fa9b3ab3f56be588a4a379bdc052ca5b
Raghul S <raghuls@Raghuls-MacBook-Air.local>
2024-04-14 09:38:14 +0530

    Initial commit

---
+++ RaghulsNeuralEngine/RaghulsNeuralEngine.h
@@ -0,0 +1,18 @@
+//
+//  RaghulsNeuralEngine.h
+//  RaghulsNeuralEngine
+//
+//  Created by Raghul S on 14/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RaghulsNeuralEngine.
+FOUNDATION_EXPORT double RaghulsNeuralEngineVersionNumber;
+
+//! Project version string for RaghulsNeuralEngine.
+FOUNDATION_EXPORT const unsigned char RaghulsNeuralEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RaghulsNeuralEngine/PublicHeader.h>
+
+
